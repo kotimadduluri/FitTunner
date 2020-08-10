04d8e6cbab71c5fbc571a70e513216ac2f55c6a2
kotimn <kotImn>
2020-08-10 23:43:45 +0530

    // 1.icons added 2.removed floatinf action

--- app/src/main/java/com/fittuner/view/home/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fittuner/view/home/ui/home/HomeFragment.kt
@@ -8,22 +8,35 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.fittuner.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.layout_nodata.*
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val homeViewModel: HomeViewModel by viewModels()
     var adapter:RunAdapter = RunAdapter()
+    lateinit var rvList: RecyclerView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val rvList: RecyclerView = view.findViewById(R.id.rvList)
+        rvList = view.findViewById(R.id.rvList)
         rvList.adapter=adapter
         /*homeViewModel.getAllRuns().observe(viewLifecycleOwner, Observer {records->
             adapter.setData(records)
         })*/homeViewModel.concertList.observe(viewLifecycleOwner, Observer {records->
             adapter.submitList(records)
+            handleNoData(records.size==0)
         })
+    }
+
+    fun handleNoData(noData: Boolean) {
+        if(noData){
+            noDataHolder.visibility=View.VISIBLE
+            rvList.visibility=View.GONE
+        }else{
+            noDataHolder.visibility=View.GONE
+            rvList.visibility=View.VISIBLE
+        }
     }
     /*fun initGoogleAdds(){
         try{
